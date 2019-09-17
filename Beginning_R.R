88500e085cdc50b4696a150390496d830303e36b
Amy Benefield <amy.benefield@colorado.edu>
2019-09-16 19:06:33 -0600

    Finished burdock data

--- Beginning_R.R
+++ Beginning_R.R
@@ -13,13 +13,50 @@
   # Compiling: try no first, then try yes, then try updating R version
   # install.packages("tidyverse", dependencies=TRUE)
   # install.packages("ggplot2", dependencies=TRUE)
+  # install.packages("dplyr", dependencies=TRUE)
+  # install.packages("plyr", dependencies=TRUE)
+
 
 ### Loading packages: library(package)
   # library(tidyverse)
   # library(ggplot2)
+  # library(dplyr)
+  #library(plyr)
 
+
+
+################## Burdock Data:
 ### Loading data:
   # No data, R data object, etc. should have spaces, or any character other than a "." or an "_"
   # Be sure your data is in the same folder as project
   # Be sure it's saved as .csv
-  # read.csv(file="data", header=TRUE)
+Burdock_data<-read.csv(file="Burdock_data.csv", header=TRUE)
+
+# Exploring Data:
+#View(Burdock_data)
+ head(Burdock_data)
+#str(Burdock_data)
+#names(Burdock_data)
+
+dat<-ddply(Burdock_data, .(Plant_number), summarize, # Summarize on Plant_number field
+        No_burrs=mean(No_burrs), # Taking mean, because they should all be the same
+        Avg_No_seeds=mean(No_seeds), # Finding average number of seeds per burr per plant
+        Plant_size=mean(Plant_size), # Taking mean, because they should all be the same
+        Num=length(Plant_number)) # Indexing number of rows collapsed for data cleaning
+View(dat)
+head(dat)
+dat$Fecundity<-dat$Avg_No_seeds*dat$No_burrs
+head(dat)
+
+### Data cleaning: who only entered the # burrs that they counted?
+which(dat$No_burrs==dat$Num) # which rows have the same total number of burrs as the number of burrs counted? Which rows are wrong?
+which(dat$No_burrs!=dat$Num) # Which rows have different values? Which are right?
+data<-dat[which(dat$No_burrs!=dat$Num),] # Let's save only those rows. Rows which only have the number of burrs counted will skew our results to show only low numbers
+
+mf<-mean(data$Fecundity)
+# Histogram
+ggplot(data, aes(Fecundity))+
+  geom_histogram(binwidth=500)+
+  geom_vline(xintercept=mean(data$Fecundity))
+
+
